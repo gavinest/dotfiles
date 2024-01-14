a23a02bbb1283951cc548c7f32dbbc11441ecc2f
Gavin Estenssoro <gavin.estenssoro@pinnacol.com>
2024-01-14 10:25:09 -0700

    install zsh & o-my-zsh

--- install.sh
+++ install.sh
@@ -4,12 +4,15 @@
 # install script for vscode devcontainers
 # ----
 
-# install chezmoi and apply dotfiles
-sh -c "$(curl -fsSL get.chezmoi.io)" -- init --apply gavinest
+# install zsh
+apt-get update && apt-get install -y zsh
 
 # install oh-my-zsh. https://github.com/deluan/zsh-in-docker
-sh -c "$(curl -ivL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
-# zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+sh -c "$(curl -ivL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" | zsh || true
+zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+
+# install chezmoi and apply dotfiles
+sh -c "$(curl -fsSL get.chezmoi.io)" -- init --apply gavinest
 
 # install vim plugins
