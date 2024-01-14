0fc27a340e34386b81ce90de4cb2ae8b7236a811
Gavin Estenssoro <gavin.estenssoro@pinnacol.com>
2024-01-14 13:28:53 -0700

    vim-plug

--- install.sh
+++ install.sh
@@ -12,10 +12,10 @@ curl -fsSL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
 
-# install chezmoi and apply dotfiles
+# install dotfiles using chezmoi
 zsh -c "$(curl -fsSL get.chezmoi.io)" -- init --apply gavinest
 
 # install vim plugins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim -c --not-a-term "PlugInstall" -c "q" -c "q"
+vim --not-a-term -c "PlugInstall" -c "q" -c "q"
