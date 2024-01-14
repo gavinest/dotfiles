4cee8f1ef95c652d5c1073f9cfd2d55704d0f50c
Gavin Estenssoro <gavin.estenssoro@pinnacol.com>
2024-01-14 09:35:13 -0700

    zsh

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/env bash
+
 # install script for vscode devcontainer
 
+# install chezmoi and apply dotfiles
 sh -c "$(curl -ivL get.chezmoi.io)" -- init --apply gavinest
+
+# install oh-my-zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+
+# install vim plugins
