ab53676ff3b921d63777e8c48ff0643527a3c733
Gavin Estenssoro <gavin.estenssoro@pinnacol.com>
2024-02-13 18:17:34 -0700

    manage hammerspoon

---
+++ dot_hammerspoon/init.lua
@@ -0,0 +1,31 @@
+-- https://www.hammerspoon.org/Spoons/
+
+shortcut_keys = { "ctrl", "alt" }
+
+hs.loadSpoon("SpoonInstall")
+spoon.SpoonInstall:andUse(
+    "Caffeine",
+    {
+        start = true,
+        hotkeys = {
+            toggle = {shortcut_keys, "c"}
+        },
+    }
+)
+
+spoon.SpoonInstall:andUse(
+    "PushToTalk",
+    {
+        start = true,
+        config = {
+          app_switcher = {["zoom.us"] = "push-to-talk"}
+        }
+    }
+)
+
+spoon.SpoonInstall.repos.ShiftIt = {
+   url = "https://github.com/peterklijn/hammerspoon-shiftit",
+   desc = "ShiftIt spoon repository",
+   branch = "master",
+}
+spoon.SpoonInstall:andUse("ShiftIt", { repo = "ShiftIt" })
