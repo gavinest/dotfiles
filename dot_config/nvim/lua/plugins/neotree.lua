1a0fa357ee0d67217bd87eb03b3f9885733bfb17
Gavin Estenssoro <gav.estenssoro@gmail.com>
2025-02-17 21:52:10 -0700

    nvim completions

--- dot_config/nvim/lua/plugins/neotree.lua
+++ dot_config/nvim/lua/plugins/neotree.lua
@@ -9,7 +9,7 @@ return {
       -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
-      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', { silent = true })
     end
   }
 }
