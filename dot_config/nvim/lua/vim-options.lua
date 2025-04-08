a04e91ed1a2aa27c99f094685d9eea8a3c18e05a
Gavin Estenssoro <gavin@Estenssoros-MacBook-Air.local>
2025-04-07 22:12:00 -0600

    nvim colorcolumn

--- dot_config/nvim/lua/vim-options.lua
+++ dot_config/nvim/lua/vim-options.lua
@@ -3,5 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.opt.clipboard = "unnamedplus"
+vim.opt.colorcolumn = "80"
 vim.g.mapleader = " "
 vim.opt.conceallevel = 1  -- allows Obsidian to use conceal features
